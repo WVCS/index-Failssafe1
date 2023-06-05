6caa3d58cc1d601fdb7d7c8819d7b24fc714dab2
Failssafe1 <123417035+Failssafe1@users.noreply.github.com>
2023-06-05 12:00:49 -0700

    bubbles

--- noun/nuon.js
+++ noun/nuon.js
@@ -5,8 +5,8 @@ canvas.height = window.innerHeight;
 
 var l = canvas.getContext('2d');
 
-
-
+// x and y are the coordinates of the circle
+// vx and vy are the respective speeds
 var x = Math.floor(Math.random() * innerWidth);
 var y = Math.floor(Math.random() * innerHeight);
 var vx = Math.floor(Math.random() * 6);
@@ -15,22 +15,22 @@ var radius = 20;
 
 move();
 
-
+// This function will do the animation
 function move() {
     requestAnimationFrame(move);
 
-    
-    
+    // It clears the specified pixels within
+    // the given rectangle
     l.clearRect(0, 0, innerWidth, innerHeight);
 
-    
+    // Creating a circle
     l.beginPath();
     l.strokeStyle = "black";
     l.arc(x, y, radius, 0, Math.PI * 2, false);
     l.stroke();
 
-    
-    
+    // Conditions so that the ball bounces
+    // from the edges
     if (radius + x > innerWidth)
         vx = 0 - vx;
 
